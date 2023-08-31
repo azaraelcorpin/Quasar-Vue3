0694443977be50f202ea902f135387693770af80
azaraelcorpin <azarael.corpin@msugensan.edu.ph>
2023-08-31 13:22:35 +0800

    initial

---
+++ spms_fe/src/layouts/MainLayout.vue
@@ -0,0 +1,165 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar style="background-color: #720206;">
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Strategic Performance Management System
+        </q-toolbar-title>
+
+        
+      </q-toolbar>
+    </q-header>
+
+    <q-footer reveal elevated style="background-color: #720206;">
+        <q-toolbar class="justify-between">
+          <div>MSU GenSan - ICTO</div>
+          <div>SPMS v{{ $version}}</div>
+        </q-toolbar>
+      </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      mini-to-overlay
+
+      :width="200"
+      :breakpoint="500"
+      bordered
+      :class="$q.dark.isActive ? 'bg-grey-9' : 'bg-grey-3'"
+    >
+     <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
+      <q-list padding>
+        <!-- <q-item v-ripple
+          header
+        >
+        <q-item-section avatar v-if="miniState">
+        <q-icon> <q-img src="~assets/MSU_Gensan_logo.png"/>
+        </q-icon></q-item-section>
+          <q-img src="~assets/MSU_Gensan_logo.png" :style="!miniState?'width: 30%;height: 30%;':''"/>
+        </q-item> -->
+
+        <!-- <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        /> -->
+
+          <q-item v-ripple>
+              <q-item-section avatar>
+                <q-icon>
+                  <q-img src="~assets/MSU_Gensan_logo.png" />
+                </q-icon>
+              </q-item-section>
+            </q-item>
+           <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="inbox" />
+              </q-item-section>
+
+              <q-item-section>
+                Inbox
+              </q-item-section>
+            </q-item>
+
+            <q-item active clickable v-ripple>
+              <q-item-section avatar>
+                <q-icon name="star" />
+              </q-item-section>
+              <q-item-section>
+                Fav
+              </q-item-section>
+              </q-item>
+
+      </q-list>
+    </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+// import EssentialLink from 'components/EssentialLink.vue'
+
+// const linksList = [
+//   {
+//     title: 'Docs',
+//     caption: 'quasar.dev',
+//     icon: 'school',
+//     link: 'https://quasar.dev'
+//   },
+//   {
+//     title: 'Github',
+//     caption: 'github.com/quasarframework',
+//     icon: 'code',
+//     link: 'https://github.com/quasarframework'
+//   },
+//   {
+//     title: 'Discord Chat Channel',
+//     caption: 'chat.quasar.dev',
+//     icon: 'chat',
+//     link: 'https://chat.quasar.dev'
+//   },
+//   {
+//     title: 'Forum',
+//     caption: 'forum.quasar.dev',
+//     icon: 'record_voice_over',
+//     link: 'https://forum.quasar.dev'
+//   },
+//   {
+//     title: 'Twitter',
+//     caption: '@quasarframework',
+//     icon: 'rss_feed',
+//     link: 'https://twitter.quasar.dev'
+//   },
+//   {
+//     title: 'Facebook',
+//     caption: '@QuasarFramework',
+//     icon: 'public',
+//     link: 'https://facebook.quasar.dev'
+//   },
+//   {
+//     title: 'Quasar Awesome',
+//     caption: 'Community Quasar projects',
+//     icon: 'favorite',
+//     link: 'https://awesome.quasar.dev'
+//   }
+// ]
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    // EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const miniState = ref(true)
+    return {
+      // essentialLinks: linksList,
+      leftDrawerOpen,
+      miniState,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
